1e78c6026f1dcfd944e7c2fd93a0afe957dbbbe8
stiller82 <info@stillermedia.de>
2024-07-10 16:51:54 +0200

    Update 10.07.2024 v2

--- php/bknd_settings.php
+++ php/bknd_settings.php
@@ -1,23 +1,46 @@
 <?php
 	global $wpdb;
+  $sm_utilities_table = sm_utilities::$table;
 
-  if($_POST["keyword_activate"]) {
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!isset($_POST['sm_utilities_nonce_field']) || !wp_verify_nonce($_POST['sm_utilities_nonce_field'], 'sm_utilities_nonce')) {
+        die('Sicherheitsprüfung fehlgeschlagen.');
+    }
+
+    $key_activate = sanitize_text_field($_POST["key_activate"]);
+    $key_deactivate = sanitize_text_field($_POST["key_deactivate"]);
+  }
+
+  if($key_activate) {
       $data = array("status" => "1");
-      $where = array("name" => $_POST["keyword_activate"]);
-      $wpdb->update(sm_utilities::$table, $data, $where);
+      $where = array("name" => $key_activate);
+
+      $cache_key = 'sm_utilities_' . $key_activate;
+      $cache_group = 'sm_utilities_group';
+
+      $wpdb->update($sm_utilities_table, $data, $where);
+
+      wp_cache_delete($cache_key, $cache_group);
       
       echo "<meta http-equiv='refresh' content='0'>";
   }
 
-  if($_POST["keyword_deactivate"]) {
+  if($key_deactivate) {
       $data = array("status" => "0");
-      $where = array("name" => $_POST["keyword_deactivate"]);
-      $wpdb->update(sm_utilities::$table, $data, $where);
+      $where = array("name" => $key_deactivate);
+
+      $cache_key = 'sm_utilities_' . $key_activate;
+      $cache_group = 'sm_utilities_group';
+
+      $wpdb->update($sm_utilities_table, $data, $where);
+
+      wp_cache_delete($cache_key, $cache_group);
       
       echo "<meta http-equiv='refresh' content='0'>";
   }
 
-  $functions = $wpdb->get_results("SELECT *  FROM " . sm_utilities::$table);
+  $prepared_query = $wpdb->prepare("SELECT * FROM $sm_utilities_table");
+  $functions = $wpdb->get_results($prepared_query);
 
   $bereiche = [
     ["Allgemein", "Allgemein"],
@@ -32,9 +55,11 @@
 <form action="#" method="post">
 
 <?php
+  wp_nonce_field('sm_utilities_nonce', 'sm_utilities_nonce_field');
+
   foreach ($bereiche as $bereich) {
     echo '
-      <h2>'.$bereich[0].'</h2>
+      <h2>'.esc_html($bereich[0]).'</h2>
         <table class="bknd_tabell">
           <tbody>';
 
@@ -42,9 +67,9 @@
       if ($function->bereich == $bereich[1]) {
         echo '
           <tr>
-            <td><button class="status_button" id="post-query-submit" class="button" type="submit" title="'.($function->status  == 1 ? "Deaktivieren" : "Aktivieren").'" name="keyword_'.($function->status  == 1 ? "deactivate" : "activate").'" value="'.$function->name.'">'.($function->status  == 1 ? "&#128994" : "&#128308").'</button></td>
-            <td>'. $function->name .'</td>
-            <td>'. $function->beschreibung .'</td>
+            <td><button class="status_button" id="post-query-submit" class="button" type="submit" title="'.($function->status  == 1 ? "Deaktivieren" : "Aktivieren").'" name="key_'.($function->status  == 1 ? "deactivate" : "activate").'" value="'.esc_html($function->name).'">'.($function->status  == 1 ? "&#128994" : "&#128308").'</button></td>
+            <td>'. esc_html($function->name) .'</td>
+            <td>'. esc_html($function->beschreibung) .'</td>
           </tr>';
       }
     }
@@ -57,4 +82,4 @@
 
 </form>
 <hr>
-<p>Copyright by <a href="https://stillermedia.de">stiller media</a> &copy; 2024 | <?php echo "Version " . sm_utilities::$version; ?></p>
+<p>Copyright by <a href="https://stillermedia.de">stiller media</a> &copy; 2024 | <?php echo "Version " . esc_html(sm_utilities::$version); ?></p>
